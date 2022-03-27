2754ebb90109684b1da667a9c1fd37df3c4d3a20
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2022-03-27 17:45:32 +0400

    commit

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -3,16 +3,29 @@ import React, { useState, useEffect } from 'react'
 const now = new Date()
 const date_ = now.toUTCString()
 function Timer() {
-  const [count, setCount] = useState(date_)
+  const [dateState, setDateState] = useState(new Date().toUTCString())
   useEffect(() => {
-    setTimeout(() => {
-      setCount((date_) => date_)
-    }, [count])
-  })
-  return <h1>I have rendered {count} times!</h1>
+    setInterval(() => setDateState(new Date().toUTCString()), 100)
+  }, [])
+  return (
+    <p>
+      {dateState.toLocaleString('', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+      })}
+    </p>
+  )
 }
 const AppHeaderDropdown = () => {
-  return <div>{date_}</div>
+  return (
+    <>
+      <div>
+        <Timer></Timer>
+      </div>
+    </>
+  )
 }
 
 export default AppHeaderDropdown
